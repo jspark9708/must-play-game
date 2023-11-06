5950673dc76eec89abcd071cd0bbf16490e530a4
JSPARK <flash246@naver.com>
2023-11-06 10:22:34 +0900

    feat: pc 게임 페이지 구현
    기능을 구현했지만 중대한 문제가 발견되어 수정이 필요함.
    
    - 기존 코드에서 발생하는 버그 수정이 필요
    - 새로운 기능에 대한 테스트 케이스 작성 필요
    관련문제: 이슈 #3

--- src/router/pc/pc.js
+++ src/router/pc/pc.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/compat/app';
-import 'firebase/compat/database'; // Ensure you have the firebase database module included
+import 'firebase/compat/database';
+import 'firebase/compat/storage';
 import firebaseConfig from '../../service/firebase.js'
-const BioshockInfo = () => {
-  const [bioshockInfo, setBioshockInfo] = useState(null);
+import styles from './pc.module.css';
+
+
+
+const PC = () => {
+  const [games, setGames] = useState();
+  const [loadedImages, setLoadedImages] = useState({});
 
   useEffect(() => {
     // Initialize Firebase app
@@ -11,38 +17,127 @@ const BioshockInfo = () => {
       firebase.initializeApp(firebaseConfig);
     }
 
-    // Reference to bioshock game data
-    const bioshockRef = firebase.database().ref('pc/bioshock');
+    // Firebase Realtime Database에서 데이터 가져오기
+    const dbRef = firebase.database().ref('games');
 
-    // Fetch Bioshock game data from Firebase
-    bioshockRef.on('value', (snapshot) => {
-      const data = snapshot.val();
-      setBioshockInfo(data);
+    dbRef.on('value', (snapshot) => {
+      const gamesData = snapshot.val();
+      
+      // 필터링된 데이터를 가져오기
+      const filteredGames = Object.keys(gamesData).reduce((acc, key) => {
+        const game = gamesData[key];
+        if (game.platform.includes('pc')) {
+          acc.push({ key, ...game });
+        }
+        return acc;
+      }, []);
+
+      setGames(filteredGames);
     });
 
-    // Unsubscribe from the data when the component unmounts
-    return () => bioshockRef.off('value');
+    return () => {
+      dbRef.off(); // 컴포넌트가 unmount될 때 listener 해제
+    };
   }, []);
+ 
+  // 이미지 로드 함수
+  // TODO : 무한 로드 해결하기
+  const loadImages = async () => {
+    const storage = firebase.storage();
+    const updatedGames = [];
 
-  // Check if bioshockInfo is being properly set
-  console.log("info:",bioshockInfo);
+    for (const game of games) {
+      if (!loadedImages[game.key]) {
+        const imageUrl = game.gameCover;
 
+        try {
+          const imageRef = storage.refFromURL(imageUrl);
+          const imageURL = await imageRef.getDownloadURL();
+          updatedGames.push({ ...game, imageUrl: imageURL });
+          setLoadedImages({ ...loadedImages, [game.key]: true });
+        } catch (error) {
+          console.error("Error fetching image:", error);
+          updatedGames.push({ ...game, imageUrl: '' });
+        }
+      } else {
+        // If the image is already loaded, use the existing URL
+        updatedGames.push({ ...game, imageUrl: game.imageUrl });
+      }
+    }
+
+    setGames(updatedGames);
+  };
+
+  // Rest of your code remains the same
+
+  useEffect(() => {
+    if (games && games.length > 0) {
+      loadImages();
+    }
+  }, [games]);
+  
   return (
-    <div>
-      <h1>Bioshock Game Information</h1>
-      {bioshockInfo ? (
-        <div>
-          <h2>{bioshockInfo.gameTitle}</h2>
-          <p>Age Rating: {bioshockInfo.ageRate}</p>
-          <p>Consoles: {bioshockInfo.consoles.join(', ')}</p>
-          <p>Description: {bioshockInfo.gameDescript}</p>
-          <a href={bioshockInfo.steamLink} target="_blank" rel="noopener noreferrer">Steam Link</a>
+    <div className={styles.bgColor}>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <h1>Must Play PC Game</h1>
+          <h3>다음에 플레이할 PC 최고의 명작 10가지를 소개해드립니다!</h3>
+          <div className={styles.separator}></div>
         </div>
-      ) : (
-        <p>Loading Bioshock information...</p>
-      )}
+        <div className={styles.listContainer}>
+          <div className={styles.feature}>
+            {/* what i played 필터, search 기능 */}
+          </div>
+          
+          <div className={styles.gameContainer}>
+            {games && games.length > 0 ? (
+            <div className={`${styles.gridContainer} ${styles.grid}`}>
+              {games.map((game) => (
+                <div key={game.key} className={styles.gameItem}>
+                  <div className={styles.gameCover}>
+                    <img src={game.imageUrl} alt={game.gameTitle} width={250}/>
+                  </div>
+                  <div className={styles.gameDetail}>
+                    <div className={styles.gameTitle}>
+                      <h3>{game.gameTitle}</h3>
+                    </div>
+                    <div className={styles.scoreContainer}>
+                      <div className={styles.scoreBox}>{game.score}</div>
+                      <p>metascore</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p>No games available</p>
+          )}
+          </div>
+          
+          {/*
+          <div className={styles.gameContainer}>
+            <div className={`${styles.gridContainer} ${styles.grid}`}>
+              <div className={styles.gameItem}>
+                <div className={styles.gameCover}>
+                  <img src={`${process.env.PUBLIC_URL}/img/game1.jpeg`}/>
+                </div>
+                <div className={styles.gameDetail}>
+                  <div className={styles.gameTitle}>
+                    <h3>Portal : the legend of zelda</h3>
+                  </div>
+                  <div className={styles.scoreContainer}>
+                    <div className={styles.scoreBox}>95</div>
+                    <p>metascore</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        */}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default BioshockInfo;
+export default PC;
