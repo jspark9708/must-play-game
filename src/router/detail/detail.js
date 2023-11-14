126b54a961d8e1fa78d7b38d8a54aabd80c52c4f
JSPARK <flash246@naver.com>
2023-11-14 14:15:21 +0900

    fix: json 수정
    json data는 local로 작동하지 않습니다. 해당 파일은 firebase에 적용하기 위해 임시적으로 둔 로컬 파일입니다.

--- src/router/detail/detail.js
+++ src/router/detail/detail.js
@@ -13,11 +13,9 @@ const Detail = (props) => {
                     {/* 초록 게임 설명 칸 */}
                     <br/><br/><br/><br/><br/>
                     <h1>{gameData.gameTitle}</h1>
-                    <h1>{gameData.gameTitle}</h1>
-                    <h1>{gameData.gameTitle}</h1>
-                    <h1>{gameData.gameTitle}</h1>
-                    <h1>{gameData.gameTitle}</h1>
-                    <h1>{gameData.gameTitle}</h1>
+                    <h1>{gameData.gameDescript}</h1>
+                    <iframe width="560" height="315" src={gameData.gameVideo} frameborder="0" allowfullscreen></iframe>
+                    <img src={gameData.gameBanner}></img>
                 </div>
                 <div className={styles.gameContainer}>
                     {/* 게임, 설명, 기타등등 요소가 들어갈 칸 */}
