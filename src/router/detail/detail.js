26ab102731ab829fd65265740d7f9384eb6a0bec
JSPARK <flash246@naver.com>
2023-11-14 16:16:37 +0900

    refactor: detail JSX 구조 변경

--- src/router/detail/detail.js
+++ src/router/detail/detail.js
@@ -3,31 +3,32 @@ import { useState, useEffect } from 'react';
 import styles from './detail.module.css';
 
 const Detail = (props) => {
-    let gameData = localStorage.getItem("itemData");
-    gameData = JSON.parse(gameData);
+    let data = localStorage.getItem("itemData");
+    data = JSON.parse(data);
 
     return (
         <div className={styles.pageContainer}>
             <div className={styles.headContents}>
-                <div className={styles.gridContainer}>
-                    {/* 초록 게임 설명 칸 */}
-                    <br/><br/><br/><br/><br/>
-                    <h1>{gameData.gameTitle}</h1>
-                    <h1>{gameData.gameDescript}</h1>
-                    <iframe width="560" height="315" src={gameData.gameVideo} frameborder="0" allowfullscreen></iframe>
-                    <img src={gameData.gameBanner}></img>
+                <div className={styles.gridContainerWide}>
+                    <p>{data.develop}</p>
                 </div>
                 <div className={styles.gameContainer}>
                     {/* 게임, 설명, 기타등등 요소가 들어갈 칸 */}
-                    <div className={styles.rightContainer}>
+                    <div className={styles.leftContainer}>
                         <div className={styles.videoArea}></div>
+                        <div className={styles.gridContainerNarrow}>
+                            <p>{data.develop}</p>
+                        </div>
                         <div className={styles.summary}></div>
                     </div>
-                    <div className={styles.leftContainer}></div>
+                    <div className={styles.rightContainer}></div>
                 </div>
             </div>
             <div className={styles.midContents}>
                 {/* 리뷰 관련 영역 */}
+            </div>
+            <div className={styles.tailContents}>
+                {/* 트위치 방송 API 관련 영역 */}
             </div>
         </div>
     );
