0e8460964afe27e91727d5f8a226cb0e362e7976
JSPARK <flash246@naver.com>
2023-11-28 00:26:18 +0900

    design: 일부 디자인 수정

--- src/components/streams/streams.jsx
+++ src/components/streams/streams.jsx
@@ -1,12 +1,11 @@
-// streams.jsx
 import React, { useEffect, useState } from "react";
 import styles from "./streams.module.css";
 import api from "../../service/api";
+import InfoIcon from "@mui/icons-material/Info";
 
 function Streams() {
   const [channels, setChannels] = useState([]);
   const [twitchGameName, setTwitchGameName] = useState(""); // State to store the Twitch game name
-
 
   const data = JSON.parse(localStorage.getItem("itemData"));
 
@@ -42,7 +41,6 @@ function Streams() {
 
         const modifiedData = await Promise.all(
           response.data.data.map(async (game) => {
-            // Fetch user data using the user_id from the stream data
             const userResponse = await api.get(
               "https://api.twitch.tv/helix/users",
               {
@@ -82,51 +80,73 @@ function Streams() {
       <div className={styles.componentArea}>
         <div className={styles.contentsTitle}>
           <h2>관련 방송</h2>
+          <div className={styles.samerow}>
+            <InfoIcon sx={{fontSize:13}}/>
+            <p>현재 트위치에서 방송중인 상위 10개의 스트림을 보여드립니다.</p>
+          </div>
         </div>
         <div className={styles.streamContainer}>
-          <ul>
-            {channels.map((channel, index) => (
-              <li key={channel.id}>
-                <div className={styles.thumbnail}>
-                  <img
-                    src={channel.thumbnail_url}
-                    alt={`Thumbnail ${index}`}
-                  ></img>
-                  <div className={styles.liveInfo}>
-                    <div>
-                      <span>생방송</span>
-                    </div>
-                    <div>
-                      <p>
-                        시청자 {channel.viewer_count.toLocaleString("ko-KR")}명
-                      </p>
+          {channels.length === 0 ? (
+            <div className={styles.forNone}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M11.64 5.93h1.43v4.28h-1.43m3.93-4.28H17v4.28h-1.43M7 2L3.43 5.57v12.86h4.28V22l3.58-3.57h2.85L20.57 12V2m-1.43 9.29l-2.85 2.85h-2.86l-2.5 2.5v-2.5H7.71V3.43h11.43Z"
+                />
+              </svg>
+              <p>현재 {data.gameTitle}을(를) 방송중인 스트리머가 없습니다.</p>
+            </div>
+          ) : (
+            <ul>
+              {channels.map((channel, index) => (
+                <li key={channel.id}>
+                  <div className={styles.thumbnail}>
+                    <img
+                      src={channel.thumbnail_url}
+                      alt={`Thumbnail ${index}`}
+                    ></img>
+                    <div className={styles.liveInfo}>
+                      <div>
+                        <span>생방송</span>
+                      </div>
+                      <div>
+                        <p>
+                          시청자 {channel.viewer_count.toLocaleString("ko-KR")}
+                          명
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className={styles.streamerInfo}>
-                  <img
-                    src={channel.profile_image_url}
-                    alt="Streamer Profile"
-                  ></img>
-                  <div className={styles.streamerInfoText}>
-                    <strong>{channel.title}</strong>
-                    <p>{channel.user_name}</p>
+                  <div className={styles.streamerInfo}>
+                    <img
+                      src={channel.profile_image_url}
+                      alt="Streamer Profile"
+                    ></img>
+                    <div className={styles.streamerInfoText}>
+                      <strong>{channel.title}</strong>
+                      <p>{channel.user_name}</p>
+                    </div>
                   </div>
+                </li>
+              ))}
+              <li>
+                <div className={styles.twitchButton}>
+                  <a
+                    href={`https://twitch.tv/directory/category/${twitchGameName}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                      <path
+                        fill="currentColor"
+                        d="M11.64 5.93h1.43v4.28h-1.43m3.93-4.28H17v4.28h-1.43M7 2L3.43 5.57v12.86h4.28V22l3.58-3.57h2.85L20.57 12V2m-1.43 9.29l-2.85 2.85h-2.86l-2.5 2.5v-2.5H7.71V3.43h11.43Z"
+                      />
+                    </svg>
+                  </a>
                 </div>
               </li>
-            ))}
-            <li>
-              <div className={styles.twitchButton}>
-                <a
-                  href={`https://twitch.tv/directory/category/${twitchGameName}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M11.64 5.93h1.43v4.28h-1.43m3.93-4.28H17v4.28h-1.43M7 2L3.43 5.57v12.86h4.28V22l3.58-3.57h2.85L20.57 12V2m-1.43 9.29l-2.85 2.85h-2.86l-2.5 2.5v-2.5H7.71V3.43h11.43Z"/></svg>
-                </a>
-              </div>
-            </li>
-          </ul>
+            </ul>
+          )}
         </div>
       </div>
     </div>
