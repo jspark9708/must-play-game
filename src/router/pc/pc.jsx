ac8d05550c6254e491d9168b1e0403a9ec821776
JSPARK <flash246@naver.com>
2023-11-30 13:56:39 +0900

    feat: 로드 기능 추가, reviwe 작성 창 수정

--- src/router/pc/pc.jsx
+++ src/router/pc/pc.jsx
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/database';
-import 'firebase/compat/storage';
-import firebaseConfig from '../../service/firebase.js'
-import styles from './pc.module.css';
-import SmartToyIcon from '@mui/icons-material/SmartToy';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import "firebase/compat/storage";
+import firebaseConfig from "../../service/firebase.js";
+import styles from "./pc.module.css";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
 
-//data 주고받기 위한 redux 
-import { useDispatch } from 'react-redux';
-import { addDetailData } from '../../store.js';
+//data 주고받기 위한 redux
+import { useDispatch } from "react-redux";
+import { addDetailData } from "../../store.js";
 
 const PC = () => {
   let navigate = useNavigate();
   const [games, setGames] = useState();
+  const [showWarning, setShowWarning] = useState(false);
   let dispatch = useDispatch();
 
   //const game에 filter
@@ -22,32 +23,39 @@ const PC = () => {
       firebase.initializeApp(firebaseConfig);
     }
 
-    const dbRef = firebase.database().ref('games');
+    const dbRef = firebase.database().ref("games");
 
-    dbRef.on('value', (snapshot) => {
+    const timeoutId = setTimeout(() => {
+      setShowWarning(true);
+    }, 5000);
+
+    dbRef.on("value", (snapshot) => {
       const gamesData = snapshot.val();
-      
+
       // 필터링된 데이터를 가져오기
       const filteredGames = Object.keys(gamesData).reduce((acc, key) => {
         const game = gamesData[key];
-        if (game.platform.includes('PC')) {
+        if (game.platform.includes("PC")) {
           acc.push({ key, ...game });
         }
         return acc;
       }, []);
 
       setGames(filteredGames);
+      clearTimeout(timeoutId); // 데이터 로딩이 완료 타이머 해제
     });
 
     return () => {
-      dbRef.off(); // 컴포넌트가 unmount될 때 listener 해제
+      // 컴포넌트가 unmount
+      dbRef.off(); // listener 해제
+      clearTimeout(timeoutId); // 타이머 해제
     };
   }, []);
-  
+
   const handleGameClick = (game) => {
     dispatch(addDetailData(game));
     navigate(`/Detail/${game.key}`);
-  }
+  };
 
   return (
     <div className={styles.bgColor}>
@@ -61,34 +69,52 @@ const PC = () => {
           <div className={styles.feature}>
             {/* what i played 필터, search 기능 */}
           </div>
-          
+
           <div className={styles.gameContainer}>
             {games && games.length > 0 ? (
-            <div className={`${styles.gridContainer} ${styles.grid}`}>
-              {games.map((game) => (
-                <div key={game.key} onClick={()=> handleGameClick(game)} className={styles.gameItem}>
-                  <div className={styles.gameImg}>
-                    <img src={game.gameCover} alt={game.gameTitle}/>
-                  </div>
-                  <div className={styles.gameDetail}>
-                    <div className={styles.gameTitle}>
-                      <h3>{game.gameTitle}</h3>
+              <div className={`${styles.gridContainer} ${styles.grid}`}>
+                {games.map((game) => (
+                  <div
+                    key={game.key}
+                    onClick={() => handleGameClick(game)}
+                    className={styles.gameItem}
+                  >
+                    <div className={styles.gameImg}>
+                      <img src={game.gameCover} alt={game.gameTitle} />
                     </div>
-                    <div className={styles.scoreContainer}>
-                      <div className={styles.scoreBox}>{game.score}</div>
-                      <p>metascore</p>
+                    <div className={styles.gameDetail}>
+                      <div className={styles.gameTitle}>
+                        <h3>{game.gameTitle}</h3>
+                      </div>
+                      <div className={styles.scoreContainer}>
+                        <div className={styles.scoreBox}>{game.score}</div>
+                        <p>metascore</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <p>No games available</p>
-          )}
+                ))}
+              </div>
+            ) : (
+              <div>
+                {showWarning ? (
+                  <div className={styles.loadComponent}>
+                    <img src={`${process.env.PUBLIC_URL}/img/load_fail.png`}></img>
+                    <p>로드 시간이 초과되었습니다. 새로고침 해주세요!</p>
+                  </div>
+                ) : (
+                  <div className={styles.loadComponent}>
+                    <img src={`${process.env.PUBLIC_URL}/img/triforce_load.gif`}></img>
+                    <p>관련 게임들을 로딩중입니다. 잠시만 기다려주세요.</p>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
         </div>
         <div className={styles.cookie}>
-          <p><SmartToyIcon/></p>
+          <p>
+            <SmartToyIcon />
+          </p>
           <p>마음에 드는 게임을 찾으셨길 바래요!</p>
         </div>
       </div>
