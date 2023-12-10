f8dd9309569fa53b513d0e4cc6c32200711ba2d7
JSPARK <flash246@naver.com>
2023-12-10 13:37:30 +0900

    fix: video 저장 위치 변경
    배포 시 public의 비디오 사이즈가 너무 커 firebase로 이동

--- src/router/main/main.jsx
+++ src/router/main/main.jsx
@@ -15,7 +15,7 @@ const Main = (props) => {
                     playsInline //모바일 환경 비디오 자동 전체화면 방지
                     style={{ width: '100%', height: 'auto'}}
                 >
-                    <source src={`${process.env.PUBLIC_URL}/video/2018-bestgame.mp4`} type="video/mp4"/>
+                    <source src="https://firebasestorage.googleapis.com/v0/b/mustplaygame.appspot.com/o/video%2F2018-bestgame.mp4?alt=media&token=8084aa56-71b0-40f4-b8c9-72909fec71c2" type="video/mp4"/>
                     Your browser dose not support the video tag.
                 </video>
             </div>
